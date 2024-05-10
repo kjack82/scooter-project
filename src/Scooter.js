33830d05e3133ee3890827bad6e009969a60463e
kjack82 <kprothero1702@gmail.com>
2024-05-10 17:42:52 +0100

    saved work for today

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,49 @@
 class Scooter {
-  // scooter code here
+
+  static nextSerial = 0
+
+  constructor (station){
+  this.station = station
+    this.user = null
+    this.serial = Scooter.nextSerial++
+    this.charge = 100
+    this.isBroken = false
+  }
+
+  rent(user){
+    if (this.charge < 20) {
+      throw new Error("scooter needs to charge")
+    } else if (this.isBroken === true) {
+      throw new Error("scooter is broken")
+     } else {
+      this.user = user
+    }
+  }
+
+  dock(station){
+    this.user = null
+    this.station = station
+  }
+  recharge (){
+    const chargingTimer = setInterval (() =>{
+if (this.charge < 100) {
+  this.charge +=10
+  console.log("Charge has increased by 10%")
+} else if (this.charge >=100) {
+console.log("Charging complete")
+clearTimeout(chargingTimer)
 }
+}, 1000)
+}
+
+requestRepair(){
+  const repair = setInterval (() => {
+    this.isBroken = false 
+    console.log("repair completed")
+    clearInterval(repair)
+  } 5000
+}
+}
+
 
 module.exports = Scooter
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,32 @@
 // require the User and Scooter classes - see where they can be used in ScooterApp.js
 
-class ScooterApp {
-  // ScooterApp code here
+const Scooter = require("./Scooter")
+
+class ScooterApp extends User {
+  constructor (username, password, age, stations, registeredUsers){
+super(username, password, age)
+    this.stations = stations
+this.registeredUsers = registeredUsers
+  }
+  registeredUser(username, password, age){
+    if (this.age < 18){
+      throw new Error `Too young to register`
+    } else if (!this.username){
+      throw new Error `already registered`
+    } else {
+      return new User
+    }
+  }
+  loginUser(username, password){
+    if (this.username !== username || this.password !== ){
+      throw new Error `Username or password is incorrect`
+    } else {
+      console.log('user has been logged in')
+    }
+  }
+  createScooter (station){
+    return this.station = new Scooter
+  }
 }
 
 module.exports = ScooterApp
